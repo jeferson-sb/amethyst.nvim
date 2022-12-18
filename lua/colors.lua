575c7f46c28d25cd6906d3d62ab9ec95e3ceda02
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-12-17 23:17:55 -0300

    feat: add color functions

--- lua/colors.lua
+++ lua/colors.lua
@@ -36,4 +36,27 @@ colors.git = {
 
 colors.statusline = colors.indigo[1]
 
+-- color functions
+local function hexToRgb(c)
+  c = string.lower(c)
+  return { tonumber(c:sub(2, 3), 16), tonumber(c:sub(4, 5), 16), tonumber(c:sub(6, 7), 16) }
+end
+
+local function blend(foreground, background, alpha)
+  alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
+  local bg = hexToRgb(background)
+  local fg = hexToRgb(foreground)
+
+  local blendChannel = function(i)
+    local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
+    return math.floor(math.min(math.max(0, ret), 255) + 0.5)
+  end
+
+  return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
+end
+
+function colors.darken(hex, amount, bg)
+  return blend(hex, bg or C.bg, amount)
+end
+
 return colors
